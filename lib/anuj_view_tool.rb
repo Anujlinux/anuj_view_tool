cb905c842dc16608adb551dddf088559f356d1e2
Anuj <mohatkaranuj@gmail.com>
2021-12-06 19:00:59 +0530

    Initial commit

---
+++ lib/anuj_view_tool.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+require_relative "anuj_view_tool/version"
+require_relative "anuj_view_tool/renderer"
+
+module AnujViewTool
+  class Error < StandardError; end
+  # Your code goes here...
+end
