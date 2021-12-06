4f833ac9f0de7decaf7af2ad2a0f4ee5cf57433a
Anuj <mohatkaranuj@gmail.com>
2021-12-06 22:36:56 +0530

    Updated readme and gitignore file

--- lib/anuj_view_tool/renderer.rb
+++ lib/anuj_view_tool/renderer.rb
@@ -1,9 +1,7 @@
 module AnujViewTool
- 	class Error < StandardError; end
-		class Renderer
-			def self.copyright name, msg
-				"&copy; #{Time.now.year} | <b>#{name}</b> #{msg}".html_safe
-			end
+	class Renderer
+		def self.copyright name, msg
+			"&copy; #{Time.now.year} | <b>#{name}</b> #{msg}".html_safe
 		end
 	end
 end
